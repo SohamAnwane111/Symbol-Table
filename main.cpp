686b7f0359c0ffa5afb6c2d0686b152552366acd
SohamAnwane111 <139840433+SohamAnwane111@users.noreply.github.com>
2024-03-14 16:16:40 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define MAX_INSTRUCTION_LENGTH 1024
+#define CAPACITY 500
 #define ASSIGN "assign"
 #define PRINT "print"
 #define BEGIN "begin"
@@ -7,9 +8,90 @@
 
 using namespace std;
 
+class Map
+{
+private:
+    pair<string, int> *arr;
+    int size;
+    int capacity;
+
+public:
+    Map(int);
+    int hash(string);
+    bool search(string);
+    bool insert(string, int);
+    int getValue(string);
+};
+
+Map ::Map(int cap = CAPACITY) : capacity(cap)
+{
+
+    size = 0;
+    arr = new pair<string, int>[capacity];
+    for (int i = 0; i < capacity; i++)
+        arr[i] = {"", INT_MIN};
+}
+
+int Map ::hash(string key)
+{
+    int hashValue = 0;
+    int factor = 3;
+    for (auto ch : key)
+    {
+        hashValue = (hashValue + ch * factor) % capacity;
+        factor *= 3;
+    }
+    return hashValue;
+}
+
+bool Map ::search(string key)
+{
+    int h = hash(key);
+    int i = h;
+
+    while (arr[i].first != "")
+    {
+        if (arr[i].first == key)
+            return true;
+        i = (i + 1) % capacity;
+        if (i == h)
+            return false;
+    }
+    return false;
+}
+
+bool Map::insert(string key, int val)
+{
+    if (size == capacity)
+        return false;
+
+    int i = hash(key);
+
+    while (arr[i].first != "" && arr[i].first != key)
+        i = (i + 1) % capacity;
+
+    if (arr[i].first == key)
+    {
+        cout << "'" << key << "' already exists" << endl;
+        return false;
+    }
+    else
+    {
+        arr[i] = {key, val};
+        size++;
+        return true;
+    }
+}
+
+int Map ::getValue(string key)
+{
+    int h = hash(key);
+    return arr[h].second;
+}
+
 typedef struct Scope
 {
-    unordered_map<string, int> symbolTable;
+    Map symbolTable;
     Scope *enclosingScope;
 } Scope;
 
@@ -57,20 +139,17 @@ int main()
             currScope = newScope;
         }
         else if (strstr(line, ASSIGN))
-        {
-            string varName = fetchVariableName(line, (char *)ASSIGN);
-            currScope->symbolTable[varName] = fetchVariableValue(line);
-        }
+            currScope->symbolTable.insert(fetchVariableName(line, (char *)ASSIGN), fetchVariableValue(line));
         else if (strstr(line, PRINT))
         {
             string varName = fetchVariableName(line, (char *)PRINT);
             Scope *scope = currScope;
 
-            while (scope && scope->symbolTable.count(varName) == 0)
+            while (scope && scope->symbolTable.search(varName) == false)
                 scope = scope->enclosingScope;
 
             if (scope)
-                cout << varName << " = " << scope->symbolTable[varName] << endl;
+                cout << varName << " = " << scope->symbolTable.getValue(varName) << endl;
             else
             {
                 cout << "error: "
